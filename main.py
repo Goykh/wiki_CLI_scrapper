f8083ff75e60f152b4a9d222176d1a363882a65f
goykh <geronymo.fuhrmann@gmail.com>
2022-11-17 23:39:57 +0100

    test before I destroy shit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import subprocess
 import sys
 import time
-
 import wikipedia
 
 # sets language to czech,
@@ -9,62 +8,90 @@ import wikipedia
 # with wikipedia.languages()
 wikipedia.set_lang('cs')
 
-
-def clear_screen():
-    """
-    Clears CMD window.
-    """
-    if sys.platform.startswith('win'):
-        subprocess.call(['cmd.exe', '/C', 'cls'])
-    else:
-        subprocess.call(['clear'])
+article_cache = {}
+options_cache = {}
+class Menu:
 
 
-def check_if_article_exists(user_input: str):
-    """
-    Takes in the user input and checks if the article exists.
-    :param user_input: str:
-    :return: article text: str
-    """
-    search = wikipedia.search(user_input)
-    for i in search:
-        if i.lower() == user_input.lower():
-            article = wikipedia.summary(user_input.lower())
-            return article
+    def show_menu(self):
+        """
+        Runs the whole app in a while loop
+        unless you stop it.
+        """
+        while True:
+            user_input = input('Zadejte název článku (nebo napište ukončit pro ukončení): ')
 
+            if user_input == 'ukončit':
+                print('Aplikace se teď ukončí!')
+                # sleeps for 1 second before shutdown
+                time.sleep(1)
+                break
 
-def menu():
-    """
-    Runs the whole app in a while loop
-    unless you stop it.
-    """
-    while True:
-        user_input = input('Zadejte název článku (nebo napište ukončit pro ukončení): ')
-        if user_input == 'ukončit':
-            print('Aplikace se teď ukončí!')
-            # sleeps for 1 second before shutdown
-            time.sleep(1)
-            break
+            try:
+                if user_input in article_cache.keys():
+                    print(article_cache.get(user_input))
 
-        result = check_if_article_exists(user_input)
+                elif user_input in options_cache.keys():
+                    print(f"Článek neexistuje, ale slovo '{user_input}' můžete nalézt v těchto článcích:")
+                    for option in options_cache.get(user_input):
+                        print(option)
+                else:
+                    result = self.check_if_article_exists(user_input)
 
-        if result:
-            print(result)
-        # if the article does not exist, it will search for the input
-        # and print a list where the word is found
-        else:
-            article_list = wikipedia.search(user_input)
+                    if result:
+                        article_cache[user_input] = result
+                        print(result)
+                    # if the article does not exist, it will search for the input
+                    # and print a list where the word is found
+                    else:
+                        article_list = wikipedia.search(user_input)
 
-            if article_list:
+                        if article_list:
+                            options_cache[user_input] = article_list
+                            print(f"Článek neexistuje, ale slovo '{user_input}' můžete nalézt v těchto článcích:")
+                            for article in article_list:
+                                print(article)
+                        # if the word is not found at all it will print about it
+                        else:
+                            print(f"Výraz '{user_input}' nebyl na stránce nalezen.")
+
+            except wikipedia.exceptions.DisambiguationError as de:
                 print(f"Článek neexistuje, ale slovo '{user_input}' můžete nalézt v těchto článcích:")
-                for i in article_list:
+                for i in de.options:
                     print(i)
-            # if the word is not found at all it will print about it
-            else:
-                print(f"Výraz '{user_input}' nebyl na stránce nalezen.")
-        input('Pro pokračování stiskněte Enter')
-        clear_screen()
+
+            except wikipedia.exceptions.WikipediaException:
+                print('Chyba! Nezadali jste text.')
+
+            input('Pro pokračování stiskněte Enter')
+
+            self.clear_screen()
+
+    @staticmethod
+    def clear_screen():
+        """
+        Clears CMD window.
+        """
+        if sys.platform.startswith('win'):
+            subprocess.call(['cmd.exe', '/C', 'cls'])
+        else:
+            subprocess.call(['clear'])
+
+    @staticmethod
+    def check_if_article_exists(user_input: str):
+        """
+        Takes in the user input and checks if the article exists.
+        :param user_input: str:
+        :return: article text: str
+        """
+        search = wikipedia.search(user_input)
+        for i in search:
+            if i.lower() == user_input.lower():
+                article = wikipedia.summary(user_input.lower())
+                return article
+
 
 
 if __name__ == '__main__':
-    menu()
+    menu = Menu()
+    menu.show_menu()
